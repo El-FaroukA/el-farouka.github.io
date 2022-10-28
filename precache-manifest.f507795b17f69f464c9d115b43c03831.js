df9cb33b3a8183f4f919e1859c7ba3729aa55a54
El-FaroukA <elfarouk.achirafi@gmail.com>
2022-10-28 18:56:50 +0200

    Updates

---
+++ precache-manifest.f507795b17f69f464c9d115b43c03831.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ee0ca0280d49e21e10",
+    "url": "/react-portfolio/static/css/main.01217791.chunk.css"
+  },
+  {
+    "revision": "31ee0ca0280d49e21e10",
+    "url": "/react-portfolio/static/js/main.31ee0ca0.chunk.js"
+  },
+  {
+    "revision": "3c5f20f2db02483f994a",
+    "url": "/react-portfolio/static/css/1.839c6f8f.chunk.css"
+  },
+  {
+    "revision": "3c5f20f2db02483f994a",
+    "url": "/react-portfolio/static/js/1.3c5f20f2.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "608f08786171313a17bd5b76c73a92ce",
+    "url": "/react-portfolio/static/media/CV.608f0878.pdf"
+  },
+  {
+    "revision": "f9746d013777144b65244259a5a0db51",
+    "url": "/react-portfolio/static/media/me2.f9746d01.png"
+  },
+  {
+    "revision": "56a4e0e195ec16d0ea84d791f5904c31",
+    "url": "/react-portfolio/static/media/me.56a4e0e1.png"
+  },
+  {
+    "revision": "e156731b2a93b71b3fa7f833bb8ff856",
+    "url": "/react-portfolio/static/media/demineur.e156731b.PNG"
+  },
+  {
+    "revision": "437ef2d1fcc87dab891aea9af8a095d9",
+    "url": "/react-portfolio/static/media/Quiz.437ef2d1.PNG"
+  },
+  {
+    "revision": "40b6f7c0d05d202acf7f5183775c351f",
+    "url": "/react-portfolio/static/media/portSaph.40b6f7c0.PNG"
+  },
+  {
+    "revision": "08b9c6e60a162b6d18cbad8f5693eeb5",
+    "url": "/react-portfolio/static/media/pokedex.08b9c6e6.PNG"
+  },
+  {
+    "revision": "b5594cf82447e91b2f4c23638247df32",
+    "url": "/react-portfolio/static/media/work.b5594cf8.svg"
+  },
+  {
+    "revision": "022831093eea0174a75bad720ff6008a",
+    "url": "/react-portfolio/static/media/school.02283109.svg"
+  },
+  {
+    "revision": "48cca00ea581f3aa53542abe2a59985e",
+    "url": "/react-portfolio/index.html"
+  }
+];
